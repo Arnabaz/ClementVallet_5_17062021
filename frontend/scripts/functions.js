9de2789d5be0313d1df083a60f7340a63ea4dc84
Clement Vallet <clement.vallet@gmail.com>
2021-11-26 23:53:56 +0100

    refactor : correction of some bugs with ID product verif

--- frontend/scripts/functions.js
+++ frontend/scripts/functions.js
@@ -21,20 +21,3 @@ function alertDisplay(referentElementQS, className, textAlert) {
     <p class=${className}>${textAlert}</p>
 `;
 }
-
-// Fonction de vérification de l'ID d'un produit
-function verifyingIdProduct(idProduct) {
-    return fetch("http://localhost:3000/api/furniture/")
-        .then((res) => res.json())
-        .then((res) => {
-            let idProducts = [];
-            for (let i = 0; i < res.length; i++) {
-                idProducts.push(res[i]._id);
-            }
-            return idProducts.includes(idProduct);
-        })
-        .catch((error) => {console.error("Un problème est survenu pendant l'appel à l'API lors de la vérification de l'ID produit", error)})
-}
-
-
-
